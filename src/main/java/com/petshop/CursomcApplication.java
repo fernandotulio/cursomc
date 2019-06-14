c998584274900466b09004645fa1227e77ad2433
fernando tulio <fernandotulio@gmail.com>
2019-06-13 21:55:35 -0300

    Criando Cliente e Endereco

--- src/main/java/com/petshop/CursomcApplication.java
+++ src/main/java/com/petshop/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.petshop.domain.Categoria;
 import com.petshop.domain.Cidade;
+import com.petshop.domain.Cliente;
+import com.petshop.domain.Endereco;
 import com.petshop.domain.Estado;
 import com.petshop.domain.Produto;
+import com.petshop.domain.enums.TipoCliente;
 import com.petshop.repositories.CategoriaRepository;
 import com.petshop.repositories.CidadeRepository;
+import com.petshop.repositories.ClienteRepository;
+import com.petshop.repositories.EnderecoRepository;
 import com.petshop.repositories.EstadoRepository;
 import com.petshop.repositories.ProdutoRepository;
 
@@ -29,6 +34,13 @@ public class CursomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -67,6 +79,19 @@ public class CursomcApplication implements CommandLineRunner{
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));		
+		
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
